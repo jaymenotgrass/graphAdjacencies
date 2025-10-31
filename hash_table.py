6a4750fd4a9466ba89e5be5d8e68fa86ed26214e
jaymenotgrass <jnotgrass22@arizonachristian.edu>
2025-10-31 09:36:40 -0700

    Update hash_table.py

--- hash_table.py
+++ hash_table.py
@@ -1,37 +1,29 @@
-class Contact:
+class Person:
     '''
-    Contact class to represent a contact with a name and number.
+    A class representing a person in a social network.
     Attributes:
-        name (str): The name of the contact.
-        number (str): The phone number of the contact.
-    '''
-    
-    pass # Delete this line when implementing the class
-
-class Node:
-    '''
-    Node class to represent a single entry in the hash table.
-    Attributes:
-        key (str): The key (name) of the contact.
-        value (Contact): The value (Contact object) associated with the key.
-        next (Node): Pointer to the next node in case of a collision.
-    '''
-   
-    pass # Delete this line when implementing the class
-
-class HashTable:
-    '''
-    HashTable class to represent a hash table for storing contacts.
-    Attributes:
-        size (int): The size of the hash table.
-        data (list): The underlying array to store linked lists for collision handling.
+        name (str): The name of the person.
+        friends (list): A list of friends (Person objects).
     Methods:
-        hash_function(key): Converts a string key into an array index.
-        insert(key, value): Inserts a new contact into the hash table.
-        search(key): Searches for a contact by name.
-        print_table(): Prints the structure of the hash table.
+        add_friend(friend): Adds a friend to the person's friend list.
+   '''
+    
+    pass # Delete this line to implement the Person class
+
+
+class SocialNetwork:
+    '''
+    A class representing a social network.
+    Attributes:
+        people (dict): A dictionary mapping names to Person objects.
+    Methods:
+        add_person(name): Adds a new person to the network.
+        add_friendship(person1_name, person2_name): Creates a friendship between two people.
+        print_network(): Prints the names of all people and their friends.
     '''
     
-    pass # Delete this line when implementing the class
+    pass # Delete this line to implement the SocialNetwork class
 
-# Test your hash table implementation here.  
+
+
+# Test your code here
