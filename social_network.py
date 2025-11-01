2cbb1df6421183b574519a2e8e02f13111938dd8
jaymenotgrass <jnotgrass22@arizonachristian.edu>
2025-11-01 10:07:29 -0700

    Debugged final output issues

--- social_network.py
+++ social_network.py
@@ -8,8 +8,15 @@ class Person:
         add_friend(friend): Adds a friend to the person's friend list.
    '''
     
-    pass # Delete this line to implement the Person class
+    def __init__(self, name):
+        self.name = name
+        self.friends = []
 
+    def add_friend(self, friend):
+        if friend not in self.friends:
+            self.friends.append(friend)
+        else:
+            print(f"{friend.name} has already been added as a friend.")
 
 class SocialNetwork:
     '''
@@ -22,8 +29,52 @@ class SocialNetwork:
         print_network(): Prints the names of all people and their friends.
     '''
     
-    pass # Delete this line to implement the SocialNetwork class
+    def __init__(self):
+        self.people = {}
+    def add_person(self, name):
+        if name not in self.people:
+            self.people[name] = Person(name)
+        else:
+            print(f"{name} has already been added.")
+    def add_friendship(self, person1_name, person2_name):
+        if person1_name in self.people and person2_name in self.people:
+            person1 = self.people[person1_name]
+            person2 = self.people[person2_name]
 
+            person1.add_friend(person2)
+            person2.add_friend(person1)
+        else:
+            print(f"Error: Friendship between {person1_name} and {person2_name} don't exist")
+    
+    def print_network(self):
+        for name, person in self.people.items():
+            friend_names = [f.name for f in person.friends]
+            print(f"{name} is friends with: {', '.join(friend_names)}")
 
 
 # Test your code here
+network = SocialNetwork()
+
+# Add people
+network.add_person("Alex")
+network.add_person("Jordan") 
+network.add_person("Morgan")
+network.add_person("Taylor")
+network.add_person("Casey")
+network.add_person("Riley")
+network.add_person("Casey")
+
+
+# Create friendships
+network.add_friendship("Alex", "Jordan")
+network.add_friendship("Alex", "Morgan")
+network.add_friendship("Jordan", "Taylor")
+network.add_friendship("Jordan", "Johnny") # "Friendship not created. Johnny doesn't exist!"
+network.add_friendship("Morgan", "Casey")
+network.add_friendship("Taylor", "Riley")
+network.add_friendship("Taylor", "Riley")
+network.add_friendship("Casey", "Riley")
+network.add_friendship("Morgan", "Riley")
+network.add_friendship("Alex", "Taylor")
+
+network.print_network()
